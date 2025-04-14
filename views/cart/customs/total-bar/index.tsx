e9b8e63ed32a5a2af7e90abddf5542a417de01aa
abdulakhatov-tech <islomabdulakhatov6975@gmail.com>
2025-04-14 16:00:42 +0500

    feat(cart): add cart

--- views/cart/customs/total-bar/index.tsx
+++ views/cart/customs/total-bar/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { useEffect, useMemo, useState } from "react";
 
@@ -5,13 +7,16 @@ import { Button } from "@/components/ui/button";
 import { useGetCart } from "@/hooks/useQueryActions/useCart";
 import { useAppSelector } from "@/hooks/useRedux";
 import type { IUser } from "@/interfaces/user.interface";
+import { LoadingSpinner } from "@/tools";
 import { formatPrice } from "@/utils/helper-fns/format-price";
 import { ShoppingCart, TicketPercent } from "lucide-react";
+import { useRouter } from "next/navigation";
 import useAuthUser from "react-auth-kit/hooks/useAuthUser";
 import Loading from "./laoding";
 
 // Memoize and refactor logic to avoid unnecessary recalculations
 const TotalBar = () => {
+	const router = useRouter();
 	const user = useAuthUser() as IUser;
 	const { data: cart, isLoading: isCartLoading } = useGetCart(user?.id);
 	const { discount: couponDiscount } = useAppSelector(
@@ -38,6 +43,16 @@ const TotalBar = () => {
 	}, [subtotal, couponDiscount]);
 
 	const couponCodeApplied = couponDiscount > 0;
+
+	const [loading, setLoading] = useState(false);
+
+	const handleProceedToCheckout = () => {
+		setLoading(true);
+		setTimeout(() => {
+			router.push("/checkout");
+			setLoading(false);
+		}, 1400);
+	};
 
 	return (
 		<div className="border p-4 rounded-sm">
@@ -66,11 +81,14 @@ const TotalBar = () => {
 					<PriceDetail label="Total" value={total} isTotal />
 				</ul>
 			)}
-			<Link href="/checkout">
-				<Button disabled={isCartLoading} className="w-full bg-[#DB4444]">
-					<TicketPercent /> Proceed to Checkout
-				</Button>
-			</Link>
+
+			<Button
+				disabled={isCartLoading}
+				className="w-full bg-[#DB4444]"
+				onClick={handleProceedToCheckout}
+			>
+				{loading ? <LoadingSpinner /> : <TicketPercent />} Proceed to Checkout
+			</Button>
 
 			<Link href="/shop">
 				<Button variant="outline" className="w-full mt-2">
